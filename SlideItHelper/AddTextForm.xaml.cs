6e8afa9279c29448771fee1b0840737a3e560d12
Devin Hero <devingreyhero@gmail.com>
2018-12-04 20:02:59 -0800

    Create basic AddTextForm page w/ inputs

--- SlideItHelper/AddTextForm.xaml.cs
+++ SlideItHelper/AddTextForm.xaml.cs
@@ -16,11 +16,11 @@ using System.Windows.Shapes;
 namespace SlideItHelper
 {
 	/// <summary>
-	/// Interaction logic for MainWindow.xaml
+	/// Interaction logic for AddTextInput.xaml
 	/// </summary>
-	public partial class MainWindow : NavigationWindow
+	public partial class AddTextInput : Page
 	{
-		public MainWindow()
+		public AddTextInput()
 		{
 			InitializeComponent();
 		}
